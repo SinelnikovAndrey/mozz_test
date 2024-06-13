fc176b7863d218ab7b3b9834254f0db5c09d09b3
andreysinelnikov <sinelnikov.mail@gmail.com>
2024-06-13 11:48:07 +0300

    change signup.dart add title, fields and button

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import '../controllers/auth.dart';
+
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
 
@@ -12,6 +14,8 @@ class SignUpPage extends StatefulWidget {
   class _SignUpPageState extends State<SignUpPage> {
     final formKey = GlobalKey<FormState>();
     final TextEditingController _nameController = TextEditingController();
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
 
 
     @override
@@ -27,7 +31,7 @@ class SignUpPage extends StatefulWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Center(
-                  child: Text("Вход",
+                  child: Text("Регистрация",
                       style: TextStyle(
                         fontFamily: 'Gilroy',
                         fontSize: 50,
@@ -36,19 +40,93 @@ class SignUpPage extends StatefulWidget {
                   ),
                 ),
                 const SizedBox(
+                  height: 50,
+                ),
+                SizedBox(
+                    width: MediaQuery.of(context).size.width * .9,
+                    child: TextFormField(
+                      validator: (value) =>
+                      value!.isEmpty ? "Укажите ваше Имя." : null,
+                      controller: _nameController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Имя"),
+                      ),
+                    )),
+                const SizedBox(
                   height: 10,
                 ),
                 SizedBox(
                     width: MediaQuery.of(context).size.width * .9,
                     child: TextFormField(
                       validator: (value) =>
-                      value!.isEmpty ? "Name cannot be empty." : null,
-                      controller: _nameController,
+                      value!.isEmpty ? "Укажите ваш Email." : null,
+                      controller: _emailController,
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(),
-                        label: Text("Name"),
+                        label: Text("Email"),
                       ),
                     )),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                    width: MediaQuery.of(context).size.width * .9,
+                    child: TextFormField(
+                      validator: (value) => value!.length < 8
+                          ? "Пароль должен иметь не меньше 8 символов."
+                          : null,
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Пароль"),
+                      ),
+                    )),
+                const SizedBox(
+                  height: 40,
+                ),
+                SizedBox(
+                    height: 65,
+                    width: MediaQuery.of(context).size.width * .9,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            createUser(
+                                _nameController.text,
+                                _emailController.text,
+                                _passwordController.text)
+                                .then((value) {
+                              if (value == "все получилось!") {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: const Text(
+                                    "Аккаунт успешно создан",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  backgroundColor: Colors.green.shade400,
+                                ));
+                                Navigator.pop(context);
+                              } else {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: Text(
+                                    value,
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                  backgroundColor: Colors.red.shade400,
+                                ));
+                              }
+                            });
+                          }
+                        },
+                        child: const Text(
+                          "Зарегистрироваться",
+                          style: TextStyle(fontSize: 16),
+                        ))),
+                const SizedBox(
+                  height: 10,
+                ),
               ],
             ),
 
