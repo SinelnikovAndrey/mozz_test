67478ef8c44e0a1729b2b81b00e79c315d2529eb
andreysinelnikov <sinelnikov.mail@gmail.com>
2024-06-13 10:46:41 +0300

    add directory 'screens' add 'home.dart' add 'signin.dart' add 'signup.dart' change 'main.dart'

--- lib/controllers/auth.dart
+++ lib/controllers/auth.dart
@@ -1,7 +1,20 @@
 import 'package:appwrite/appwrite.dart';
 
-Client client = Client();
-client
+Client client = Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('666053020014891fd972')
-    .setSelfSigned(status: true); // For self signed certificates, only use for development
+    .setSelfSigned(
+        status: true); // For self signed certificates, only use for development
+
+Account account = Account(client);
+
+// Create a new user using email
+Future<String> createUser(String name, String email, String password) async {
+  try {
+    await account.create(
+        userId: ID.unique(), email: email, password: password, name: name);
+    return "все получилось!";
+  } on AppwriteException catch (e) {
+    return e.message.toString();
+  }
+}
